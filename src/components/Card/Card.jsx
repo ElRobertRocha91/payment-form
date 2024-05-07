e45b8792b57d1715b66ac4c755e89dd6862b69b9
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2024-05-06 22:24:21 -0300

    update card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,43 @@
-import style from "./Card.module.css";
+import "./Card.module.css";
+import imageChip from "../../assets/chip.png";
+import imageVisa from "../../assets/visa.png";
 
 function Card(){
     return (
-        <div></div>
+        <div className="container">
+            <div className="card-container">
+                {/* Front - Card */}
+                <div className="front">
+                    <div>
+                        <img src={imageChip} alt="chip"/>
+                        <img src={imageVisa} alt="visa"/>
+                    </div>
+                    {/* Data - Card */}
+                    <div>################</div>
+                    <div className="flexbox">
+                        <div className="box">
+                            <span>card holder</span>
+                            <div>full name</div>
+                        </div>
+                        <div className="box">
+                            <span>expires</span>
+                            <div>
+                                <span>mm</span>
+                                <span>yy</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Back - Card */}
+                <div className="back">
+                    <div className="stripe"></div>
+                    <div className="box">
+                        <span>cvv</span>
+                        <img src={imageVisa} alt="visa"/>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
